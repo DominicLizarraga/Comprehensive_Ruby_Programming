9daf92e266c4fc767b5c72cfa3e4609133219a13
Dominic Lizarraga <70678718+DominicLizarraga@users.noreply.github.com>
2022-10-09 23:54:33 -0700

    first variable exercise

---
+++ comprehensive_ruby.rb
@@ -0,0 +1,9 @@
+puts "hello world!" # print only the message, the program is not longer running
+
+p "hello world!" # prints the whole, it works for debugg
+
+array = ['a', 'b', 'c']
+
+puts array # a b c
+
+p array # ['a', 'b', 'c']
