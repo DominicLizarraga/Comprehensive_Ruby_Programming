54a12d6071a7a63ad9a078a282cc0b8788b92751
Dominic Lizarraga <70678718+DominicLizarraga@users.noreply.github.com>
2022-10-11 23:32:59 -0700

    math ruby

---
+++ math_ruby.rb
@@ -0,0 +1,22 @@
+# Please Excuse My Dear Aunt Sally
+# PEDMAS
+# Parentesis, Exponents, Multiplication, Division Additon, Subtraction
+
+p 5 + 15 * 20 - 2 / 6**3 - (3 - 1) # 301
+# 5 + 15 * 20 - 2 / 6**3 - 4
+# 5 + 15 * 20 - 2 / 216 - 4
+# 5 + 300 - 2 / 216 - 4
+# 5 + 300 - 0 - 4
+# 305 - 0 - 4
+# 301
+
+
+Integer (1, 2, 3)
+Float (1.2, 2.4, 3.4)
+Decimal (1.283643)
+
+
+
+
+
+
