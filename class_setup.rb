4e9826891e163eb653eb360c73cacabce7d487ee
Dominic Lizarraga <70678718+DominicLizarraga@users.noreply.github.com>
2022-11-06 00:08:46 -0700

    inheritance ruby

--- class_setup.rb
+++ class_setup.rb
@@ -16,6 +16,21 @@ class ApiConnector
 end
 
 
+class SmsConnector < ApiConnector
+
+  def sending_sms
+    puts "sending sms...."
+  end
+end
+
+class PhoneConnector < ApiConnector
+  def sending_phone_call
+    puts "sending phone call...."
+  end
+end
+
+
+
 api = ApiConnector.new(title: "My title", description: "my cool description", url: "yahoo.com")
 
 api.testing_initializers
@@ -23,3 +38,29 @@ api.testing_initializers
 # My title
 # my cool description
 # yahoo.com
+
+
+# now using inheritance with different subclasses
+
+
+phone = PhoneConnector.new(title: "My title from Phone", description: "my cool description from Phone", url: "yahoo.com")
+
+phone.sending_phone_call
+
+
+# My title from Phone
+# my cool description from Phone
+# yahoo.com
+
+
+sms = SmsConnector.new(title: "My title from Sms", description: "my cool description from Sms", url: "yahoo.com")
+
+sms.sending_sms
+
+
+
+
+# My title from Sms
+# my cool description from Sms
+# yahoo.com
+
